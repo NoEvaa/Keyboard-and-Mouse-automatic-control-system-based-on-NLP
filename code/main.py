05fa2397b7cfd120e336518d646799c16cb35cec
NoEvaa <62903279+NoEvaa@users.noreply.github.com>
2021-11-23 00:07:01 +0800

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Welcome to Noah's Ark
+"""
+#####
+#Here you can add your extension_model
+
+#from yZextension_example import *
+
+######
+
+from yZmainui import YzMainUi
+ui = YzMainUi()
+ui.master.iconbitmap('Icon.ico')
+ui.mainloop()
